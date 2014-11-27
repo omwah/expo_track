ca8e0019db6fec28d203e91f54bbf576ae91693b
Omwah <omwah@oyeh.org>
2014-11-26 16:56:48 -0800

    Move login javascript to a seperate file and fix issue with login window flashing on reload

--- expo_track/static/js/interface.js
+++ expo_track/static/js/interface.js
@@ -1,5 +1,8 @@
+// Place helper routine in global space
+var json_request;
+
 $( document ).ready(function() {
-    var json_request = function(uri, method, data) {
+    json_request = function(uri, method, data) {
         var request = $.ajax({ url: uri,
                                type: method,
                                contentType: "application/json",
@@ -10,60 +13,5 @@ $( document ).ready(function() {
                              });
         return request;
     }
-
-    function LoginViewModel() {
-        var self = this;
-        self.username = ko.observable();
-        self.password = ko.observable();
-        self.remember_me = ko.observable();
-        self.authenticated = ko.observable(false);
-
-        self.login_uri = $loginView.find("form")[0].action;
-
-        self.login = function(data) {
-            json_request(self.login_uri, "POST", data).then(
-                function(ret_data, textStatus, jqXHR) {
-                    // .done()
-                    self.authenticated(true);
-                    self.password("");
-                },
-                function(jqXHR, textStatus, errorThrown) {
-                    // .failed()
-                    self.authenticated(false);
-                    self.password("");
-                }
-            );
-        }
-
-        self.logout = function(data) {
-            json_request(self.login_uri, "DELETE").done(
-                function(ret_data) {
-                    self.authenticated(false);
-                    self.username("");
-                    self.password("");
-                }
-            );
-        }
-
-        self.check_auth = function() {
-            json_request(self.login_uri, "GET").then(
-                function(ret_data, textStatus, jqXHR) {
-                    // .done()
-                    self.authenticated(true);
-                    self.username(ret_data['username']);
-                },
-                function(jqXHR, textStatus, errorThrown) {
-                    // .failed()
-                    self.authenticated(false);
-                    $loginView.css("display", "block")
-                }
-            );
-        }
-
-        self.check_auth();
-    }
     
-    var $loginView = $("#login-view");
-    var loginViewModel = new LoginViewModel();
-    ko.applyBindings(loginViewModel);
 });
