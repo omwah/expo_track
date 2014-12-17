813d7ea7a8159efa5f69083392873eaf75258f8f
Omwah <omwah@oyeh.org>
2014-12-17 13:06:29 -0800

    Update example data to include contacts

--- example_data.py
+++ example_data.py
@@ -7,6 +7,7 @@ import random
 from datetime import datetime, timedelta
 
 from expo_track.item.constants import *
+from expo_track.person.constants import *
 
 # Administrator account
 admin_person = models.Person(given_name='Admin')
@@ -79,36 +80,48 @@ for radio_num in range(1, 61):
     items.append(radio)
     db.session.add(radio)
 
-# Random people from 
-# http://listofrandomnames.com/
-random_names = [ "Kenton Blackwood",
-                 "Delphine Dilbeck",
-                 "Kellye Mello",
-                 "Jayson Gaylor",
-                 "Brooks Rone",
-                 "Rubi Brumit",
-                 "Ila Arvidson",
-                 "Carita Sillman",
-                 "Elida Winget",
-                 "John Hartford",
-                 "Rosa Carrier",
-                 "Chanel Mcivor",
-                 "Cassidy Garbarino",
-                 "Lorrie Mcnamee",
-                 "Euna Schutt",
-                 "Regan Hocking",
-                 "January Escareno",
-                 "Carola Schenck",
-                 "Lyndsay Mackowiak",
-                 "Reyes Luedke",]
+# Ranom people and names from:
+# http://www.generatedata.com/
+example_people = [
+    {"name": "Sierra Cotton", "address": "5095 Vitae, St.", "city": "Fort Smith", "region": "AR", "postal_code": "72748", "phone_number": "(190) 891-9585", "email_address": "vitae.diam.Proin@ligula.org"},
+    {"name": "Cheryl Bridges", "address": "P.O. Box 829, 2967 Ornare, Av.", "city": "Pocatello", "region": "ID", "postal_code": "80965", "phone_number": "(485) 639-4755", "email_address": "fringilla@In.edu"},
+    {"name": "Hedda Drake", "address": "629 Et Ave", "city": "San Jose", "region": "CA", "postal_code": "96261", "phone_number": "(672) 832-8491", "email_address": "arcu.Vivamus.sit@loremluctus.co.uk"},
+    {"name": "Daryl Lowe", "address": "P.O. Box 420, 9630 Urna, St.", "city": "Los Angeles", "region": "CA", "postal_code": "95611", "phone_number": "(651) 203-8908", "email_address": "aliquam.iaculis.lacus@egetmetus.org"},
+    {"name": "Mia Combs", "address": "7596 Felis Road", "city": "Gaithersburg", "region": "MD", "postal_code": "49013", "phone_number": "(279) 108-7747", "email_address": "pede.ac.urna@arcuvelquam.co.uk"},
+    {"name": "Kaitlin Browning", "address": "Ap #442-285 Nec Avenue", "city": "Springfield", "region": "MA", "postal_code": "54992", "phone_number": "(614) 131-1260", "email_address": "consequat@DonecestNunc.co.uk"},
+    {"name": "Prescott Cooper", "address": "270-1590 Scelerisque, Av.", "city": "Dallas", "region": "TX", "postal_code": "64049", "phone_number": "(116) 207-1989", "email_address": "eu.lacus@Phasellus.org"},
+    {"name": "Gillian Holt", "address": "2959 Eu Rd.", "city": "Covington", "region": "KY", "postal_code": "26233", "phone_number": "(493) 935-8852", "email_address": "lacinia.mattis@euultrices.ca"},
+    {"name": "Isabelle Hobbs", "address": "2627 Enim, Rd.", "city": "Mesa", "region": "AZ", "postal_code": "85522", "phone_number": "(788) 844-0529", "email_address": "nunc.interdum.feugiat@mauris.edu"},
+    {"name": "Aretha Brooks", "address": "191-6357 Suspendisse Rd.", "city": "Pittsburgh", "region": "PA", "postal_code": "73468", "phone_number": "(130) 140-6835", "email_address": "vel@et.ca"},
+    {"name": "Graham Rhodes", "address": "970-2462 Lacus. Street", "city": "Philadelphia", "region": "PA", "postal_code": "80247", "phone_number": "(820) 427-7363", "email_address": "a.neque@molestie.com"},
+    {"name": "Olga Wilder", "address": "P.O. Box 573, 9567 Arcu. Street", "city": "Madison", "region": "WI", "postal_code": "62744", "phone_number": "(554) 530-1619", "email_address": "Nullam.scelerisque.neque@augue.net"},
+    {"name": "Mechelle Steele", "address": "698-4684 Aenean Av.", "city": "Fairbanks", "region": "AK", "postal_code": "99607", "phone_number": "(696) 407-3830", "email_address": "quis@nibh.co.uk"},
+    {"name": "Erin Cameron", "address": "P.O. Box 310, 4354 Dictum Av.", "city": "Frederick", "region": "MD", "postal_code": "65636", "phone_number": "(384) 797-3378", "email_address": "at.fringilla.purus@Curabiturvel.edu"},
+    {"name": "Duncan Reyes", "address": "634-8651 Non Street", "city": "Seattle", "region": "WA", "postal_code": "39076", "phone_number": "(554) 370-5671", "email_address": "Cras@ac.co.uk"},
+    {"name": "Harrison Lane", "address": "P.O. Box 202, 2905 Diam St.", "city": "Joliet", "region": "IL", "postal_code": "66365", "phone_number": "(648) 433-1267", "email_address": "bibendum@lobortisrisus.edu"},
+    {"name": "Shelly Sawyer", "address": "4468 Vulputate Road", "city": "Bridgeport", "region": "CT", "postal_code": "51585", "phone_number": "(934) 497-7874", "email_address": "tincidunt.neque.vitae@Morbiquisurna.edu"},
+    {"name": "Pandora Goff", "address": "Ap #126-857 Tempor Rd.", "city": "Jefferson City", "region": "MO", "postal_code": "17416", "phone_number": "(988) 985-4171", "email_address": "Sed@Aeneanmassa.com"},
+    {"name": "Debra Kelley", "address": "P.O. Box 839, 9887 Semper Ave", "city": "Sterling Heights", "region": "MI", "postal_code": "85096", "phone_number": "(342) 222-6330", "email_address": "mus.Proin@ullamcorperDuis.edu"},
+    {"name": "Burke Hughes", "address": "P.O. Box 276, 5764 Donec Street", "city": "Jackson", "region": "MS", "postal_code": "43882", "phone_number": "(547) 335-1842", "email_address": "molestie.Sed.id@nequeNullam.ca"},
+]
 
-people_per_team = len(random_names) / len(teams_dict.keys())
+people_per_team = len(example_people) / len(teams_dict.keys())
 people = []
 for team in teams_dict.values():
     for count in range(people_per_team):
-        curr_name = random_names.pop()
-        given, family = curr_name.split()
+        curr_person = example_people.pop()
+
+        given, family = curr_person['name'].split()
         person = models.Person(given_name=given, family_name=family)
+
+        contact_type = random.randrange(len(CONTACT_TYPES))
+        curr_address = "%s\n%s, %s %s" % ('\n'.join(curr_person['address'].split(', ')), 
+                curr_person['city'], curr_person['region'], curr_person['postal_code'])
+        contact = models.Contact(type=contact_type, 
+                address=curr_address, phone_number=curr_person['phone_number'], 
+                email_address=curr_person['email_address'])
+        person.contacts.append(contact)
+
         people.append(person)
         db.session.add(person)
 
