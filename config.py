cd1872a1fe3bfcd713e50036e4b6f326fdf3b71f
Omwah <omwah@oyeh.org>
2014-12-17 20:21:25 -0800

    Add deployment configs and update README

--- config.py
+++ config.py
@@ -7,7 +7,11 @@ INSTANCE_DIR = os.path.join(_base_dir, "instance")
 DEBUG = True
 
 # Site administrators contact information, by default just <username>@<hostname>
-ADMINS = frozenset(['%s@%s' % (os.getlogin(), platform.node())])
+try:
+    ADMINS = frozenset(['%s@%s' % (os.getlogin(), platform.node())])
+except OSError as exc:
+    # In case os.getlogin() fails
+    ADMINS = frozenset(['%s@%s' % (os.environ['USER'], platform.node())])
 
 # Used to sign cookies, change for production
 SECRET_KEY = 'This string will be replaced with a proper key in production.'
